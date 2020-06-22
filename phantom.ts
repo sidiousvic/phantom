d98a62be6a3ad9e1e58937552084fc88e5d8987f
sidiousvic <victor6rene@gmail.com>
2020-06-23 00:13:07 +0900

    phantomDOM

--- phantom.ts
+++ phantom.ts
@@ -1,4 +1,17 @@
+type PseudoNode = {
+  tagName: string;
+  attributes: { id: string; class: string | DOMTokenList };
+  $children: PseudoNode[];
+  innerHTML: string;
+};
+
+type XDOMFunction = {
+  (): [string, any];
+};
+
 function PHANTOM(reduxStore: any, XDOM: XDOMFunction) {
+  let phantomDOM: any = { test: { attributes: { id: 0 }, dataset: {} } };
+
   function LAUNCHDOM() {
     const body = document.body;
     if (!document.querySelector("#PHANTOM")) {
@@ -7,59 +20,101 @@ function PHANTOM(reduxStore: any, XDOM: XDOMFunction) {
       body?.appendChild(PHANTOM);
     }
     document.getElementById("X")?.firstChild;
-    const DOM = RENDER();
+    const DOM = RENDERPSEUDOELEMENT();
     SWAPNODE(DOM, document.querySelector("#PHANTOM"));
     return DOM;
   }
 
   function COALESCEXDOM() {
     return `
-      <div id="PHANTOM">
-      ${XDOM()}
-      </div>
-      `;
+    <div id="PHANTOM">
+    ${XDOM()}
+    </div>
+    `;
+  }
+
+  function RENDERPSEUDOELEMENT(
+    pseudoElement = TRANSMUTEXMLTOPSEUDOEL(COALESCEXDOM())
+  ) {
+    const {
+      tagName,
+      attributes,
+      innerHTML,
+      children /*dataset*/,
+    } = pseudoElement;
+
+    let $children: PseudoNode[] = [];
+
+    if (children && children.length) {
+      ($children as unknown) = Array.prototype.map.call(children, (child) => {
+        RENDERPSEUDOELEMENT(child);
+      });
+    }
+
+    let DOMElement;
+
+    // dom diffing
+    Object.values(phantomDOM).map((phantomNode: any) => {
+      if (
+        phantomNode.attributes.id === pseudoElement.attributes.id &&
+        JSON.stringify(phantomNode.dataset) !==
+          JSON.stringify(pseudoElement.dataset)
+      ) {
+        let newNode = document.createElement(tagName);
+        for (const [k, v] of Object.entries(attributes)) {
+          newNode.setAttribute(k, v as string);
+        }
+        newNode.innerHTML = innerHTML;
+        let targetNode = document.querySelector(`#${attributes.id}`);
+        SWAPNODE(newNode, targetNode);
+        DOMElement = newNode;
+      }
+    });
+
+    phantomDOM[attributes.id] = pseudoElement;
+    DOMElement = document.createElement(tagName);
+
+    for (const [k, v] of Object.entries(attributes)) {
+      DOMElement.setAttribute(k, v as string);
+    }
+
+    DOMElement.innerHTML = innerHTML;
+    return DOMElement;
   }
 
   function TRANSMUTEXMLTOPSEUDOEL(xml: string) {
     if (typeof xml !== "string") xml = (xml as HTMLElement).outerHTML;
     let doc = new DOMParser().parseFromString(xml, "text/html");
     const $el = doc.body.firstChild;
-    const { tagName, children, id, classList, innerHTML } = $el as HTMLElement;
+    const {
+      tagName,
+      children,
+      id,
+      dataset,
+      classList,
+      innerHTML,
+      outerHTML,
+    } = $el as HTMLElement;
+
     let $children: PseudoNode[] = [];
+
     if (children && children.length) {
       ($children as unknown) = Array.prototype.map.call(children, (child) => {
         return TRANSMUTEXMLTOPSEUDOEL(child);
       });
     }
+
     return {
       tagName,
       attributes: {
         id,
         class: classList,
       },
-      $children,
+      dataset,
+      children: $children,
       innerHTML,
+      outerHTML,
     };
-  }
-
-  function RENDERPSEUDOELEMENT({ tagName, attributes, innerHTML }: PseudoNode) {
-    const ELEMENT = document.createElement(tagName);
-
-    for (const [k, v] of Object.entries(attributes)) {
-      ELEMENT.setAttribute(k, v as string);
-    }
-
-    ELEMENT.innerHTML = innerHTML;
-
-    return ELEMENT;
-  }
-
-  function RENDER() {
-    const element = TRANSMUTEXMLTOPSEUDOEL(COALESCEXDOM());
-    if (typeof element === "string") {
-      return document.createTextNode(element);
-    }
-    return RENDERPSEUDOELEMENT(element);
   }
 
   function SWAPNODE(
@@ -71,7 +126,7 @@ function PHANTOM(reduxStore: any, XDOM: XDOMFunction) {
   }
 
   reduxStore.subscribe(() => {
-    SWAPNODE(LAUNCHDOM(), document.getElementById("X")?.firstChild);
+    RENDERPSEUDOELEMENT();
   });
 
   return {
