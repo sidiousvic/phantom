80ee8d68e49e4f18e87ca548a15d001e16308008
sidiousvic <victor6rene@gmail.com>
2020-06-24 00:50:45 +0900

    updated tests

--- __tests__/phantom.test.js
+++ __tests__/phantom.test.js
@@ -48,8 +48,8 @@ test("DOM is updated after firing a state change", () => {
   function phantomDOM() {
     const { title } = data();
     return `
-    <div data-phantom="${title}" id="title-div">
-      <h1 id="title-h1">${title}</h1>
+    <div id="title-div">
+      <h1 data-phantom="${title}" id="title-h1">${title}</h1>
     </div>
     `;
   }
@@ -64,14 +64,45 @@ test("DOM is updated after firing a state change", () => {
   }
 
   // click title element
+  const toBeSwappedOut = document.getElementById("title-h1");
+  toBeSwappedOut.click();
+  const swappedIn = document.getElementById("title-h1");
+
+  expect(swappedIn.innerHTML).toBe("JUST DO SHIT");
+});
+
+test("PHANTOM DOM is properly rendered", () => {
+  // init phantomDOM
+  const { fire, data, launch } = phantom(reduxStore, phantomDOM);
+  function phantomDOM() {
+    const { title } = data();
+    return `
+    <div id="title-div">
+      <h1 data-phantom="${title}" id="title-h1">${title}</h1>
+    </div>
+    `;
+  }
+  launch();
+
+  // add listener
+  document.addEventListener("click", justDoShit);
+  function justDoShit(e) {
+    if (e.target.id === "title-h1") {
+      fire({ type: "TOGGLE_TITLE" });
+    }
+  }
+
+  // get PHANTOM element's (actual element in the DOM) innerHTML, trim()
+  const phantomElementInnerHTML = document
+    .getElementById("PHANTOM")
+    .innerHTML.trim();
+
+  // click title element
   const titleH1 = document.getElementById("title-h1");
   titleH1.click();
 
-  const expected = `
-    <div data-phantom="JUST DO SHIT" id="title-div">
-      <h1 id="title-h1">JUST DO SHIT</h1>
-    </div>
-    `;
+  // get phantomDOM's (as returned by phantomDOM) HTML, trim()
+  const phantomDOMHTML = phantomDOM().trim();
 
-  expect(phantomDOM()).toBe(expected);
+  expect(phantomElementInnerHTML).toBe(phantomDOMHTML);
 });
