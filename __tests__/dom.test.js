1155b67a2c25f92bda2e3438a5649f1c329176a5
sidiousvic <victor6rene@gmail.com>
2020-07-08 19:38:37 +0900

    dom test file

---
+++ __tests__/dom.test.js
@@ -0,0 +1,89 @@
+import phantom from "../dist/phantom";
+import reduxStore from "./utils/reduxStore";
+
+/*
+ * Test Phantom's DOM output
+ */
+
+test("The PHANTOM element is rendered and wraps around the application", () => {
+  // init phantomElement
+  const { launch } = phantom(reduxStore, phantomElement);
+  function phantomElement() {
+    return `
+      <div>
+        <h1>PHANTOM</h1>
+      </div>
+      `;
+  }
+  launch();
+
+  const PHANTOMEl = document.body.firstChild;
+
+  expect(PHANTOMEl.id).toBe("PHANTOM");
+});
+
+test("DOM is updated after firing a state change", () => {
+  // init phantomElement
+  const { fire, data, launch } = phantom(reduxStore, phantomElement);
+  function phantomElement() {
+    const { title } = data();
+    return `
+      <div id="title-div">
+        <h1 data-phantom="${title}" id="title-h1">${title}</h1>
+      </div>
+      `;
+  }
+  launch();
+
+  // add listener
+  document.addEventListener("click", justDoShit);
+  function justDoShit(e) {
+    if (e.target.id === "title-h1") {
+      fire({ type: "TOGGLE_TITLE" });
+    }
+  }
+
+  // click title element
+  const toBeSwappedOut = document.getElementById("title-h1");
+  toBeSwappedOut.click();
+  const swappedIn = document.getElementById("title-h1");
+
+  expect(swappedIn.innerHTML).toBe("JUST DO SHIT");
+});
+
+test("PHANTOM DOM is properly rendered", () => {
+  // init phantomElement
+  const { fire, data, launch } = phantom(reduxStore, phantomElement);
+  function phantomElement() {
+    const { title } = data();
+    return `
+      <div id="title-div">
+        <h1 id="title-h1">${title}</h1>
+      </div>
+      `;
+  }
+
+  launch();
+
+  // add listener
+  document.addEventListener("click", justDoShit);
+  function justDoShit(e) {
+    if (e.target.id === "title-h1") {
+      fire({ type: "TOGGLE_TITLE" });
+    }
+  }
+
+  // get dom element's innerHTML, trim()
+  const domElementInnerHTML = document
+    .getElementById("PHANTOM")
+    .innerHTML.trim();
+
+  // click title element
+  const titleH1 = document.getElementById("title-h1");
+  titleH1.click();
+
+  // get phantomElement's (as returned by phantomElement) HTML, trim()
+  const phantomElementHTML = phantomElement().trim();
+
+  expect(domElementInnerHTML).toBe(phantomElementHTML);
+});
