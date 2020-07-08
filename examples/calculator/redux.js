87033f584d4b84ce5e9f4097da8ecf8dfbb86257
Nayely Rodarte <nayelyrodarte@gmail.com>
2020-07-07 19:10:49 -0500

    equals operator no longer showing on screen

--- examples/calculator/redux.js
+++ examples/calculator/redux.js
@@ -24,19 +24,29 @@ function reducer(state = initialState, action) {
       };
     case "REGISTER_N1":
       let n1 = parseFloat([...state.screen].join(""));
-      return {
-        ...state,
-        screen: [state.screen.join(""), action.operator],
-        n1,
-        operator: action.operator,
-        state: "calculating",
-      };
+      if (action.operator === "equals") {
+        return {
+          ...state,
+          screen: [state.screen],
+          n1,
+          operator: action.operator,
+          state: "calculating",
+        };
+      } else {
+        return {
+          ...state,
+          screen: [state.screen.join(""), action.operator],
+          n1,
+          operator: action.operator,
+          state: "calculating",
+        };
+      }
     case "CALCULATE":
       let result = [];
       console.log(state.screen.join("").split());
       let n2 = parseFloat(state.n2); // turn what's on the screen to n2
-      if (isNaN(n2)) n2 = state.n1; // if no n2 was selected, operate by itself
-      console.log(state.n1, n2);
+      //if (NaN(n2)) n2 = state.n1; // if no n2 was selected, operate by itself
+      // console.log(state.n1, n2);
       switch (state.operator) {
         case "+":
           result = state.n1 + n2;
@@ -50,8 +60,17 @@ function reducer(state = initialState, action) {
         case "*":
           result = state.n1 * n2;
           break;
+        case "equals":
+          result = state.n1;
+          break;
       }
-      return { ...state, screen: [result], n1: null, state: "calculated" };
+      return {
+        ...state,
+        screen: [result],
+        n1: null,
+        n2: null,
+        state: "calculated",
+      };
     case "RESET":
       return { ...state, screen: ["0"], n1: null, state: "calculated" };
     default:
