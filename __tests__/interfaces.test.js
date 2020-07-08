54d265e40c399d2fa5b1755898b2f458dcab8311
sidiousvic <victor6rene@gmail.com>
2020-07-08 19:38:48 +0900

    interfaces test file

---
+++ __tests__/interfaces.test.js
@@ -0,0 +1,30 @@
+import phantom from "../dist/phantom";
+import reduxStore from "./utils/reduxStore";
+
+/*
+ * Test the Phantom engine's interfaces
+ */
+
+test("the 'fire' interface points to the reduxStore dispatch", () => {
+  const { fire } = phantom(reduxStore, phantomElement);
+  function phantomElement() {
+    return ``;
+  }
+  expect(fire).toBe(reduxStore.dispatch);
+});
+
+test("the 'data' interface points to the reduxStore getState", () => {
+  const { data } = phantom(reduxStore, phantomElement);
+  function phantomElement() {
+    return ``;
+  }
+  expect(data).toBe(reduxStore.getState);
+});
+
+test("the 'launch' interface returns an HTMLDivElement instance", () => {
+  const { launch } = phantom(reduxStore, phantomElement);
+  function phantomElement() {
+    return ``;
+  }
+  expect(launch() instanceof HTMLDivElement).toBe(true);
+});
