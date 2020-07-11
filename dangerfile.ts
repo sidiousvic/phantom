9dd7230e42a8623a28a2e176a53243b1c052e14c
sidiousvic <victor6rene@gmail.com>
2020-07-11 12:34:41 +0900

    sets gander

--- dangerfile.ts
+++ dangerfile.ts
@@ -5,7 +5,7 @@ import { markdown } from "danger";
 //   .map((fileName) => `🍍&nbsp;&nbsp;\`${fileName}\``)
 //   .join("<br>");
 markdown(
-  "**<h4>Sick human, you opened a PR against `production`!</h4><br>Hop outta here before I destroy you with my alien frog lasers! 🐸💥**"
+  "**<h4>Sick human, you opened a PR against `production`!</h4> <br> Hop outta here before I destroy you with my alien frog lasers! 🐸 💥**"
 );
 
 // this is x test.
