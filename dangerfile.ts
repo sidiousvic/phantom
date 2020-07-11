3aa882ab215e993f863563b9239ba991fb5de6c8
sidiousvic <victor6rene@gmail.com>
2020-07-11 14:10:31 +0900

    danger testes

--- dangerfile.ts
+++ dangerfile.ts
@@ -2,17 +2,15 @@ import { danger, markdown } from "danger";
 
 const baseBranch = danger.github.pr.base.ref;
 
-switch (baseBranch) {
+if (baseBranch === "production") {
   // when opening a PR against the production branch
-  case "production":
-    markdown(
-      `<h3>Sick human, you have opened a PR against <code>${baseBranch}</code>!</h3>Hop outta here before I destroy you with my alien frog lasers! 🐸&nbsp;&nbsp;💥`
-    );
-  case "dev":
-    // when opening a PR against the dev branch
-    markdown(
-      `<h3>Good, good... you have opened a PR against <code>${baseBranch}</code>.</h3>Go fix yourself a pina colada while the checks pass and someone reviews your code. 🐸&nbsp;&nbsp;🍍`
-    );
-}
+  markdown(
+    `<h3>Sick human, you have opened a PR against <code>${baseBranch}</code>!</h3>Hop outta here before I destroy you with my alien frog lasers! 🐸&nbsp;&nbsp;💥`
+  );
+} else if (baseBranch === "dev")
+  // when opening a PR against the dev branch
+  markdown(
+    `<h3>Good, good... you have opened a PR against <code>${baseBranch}</code>.</h3>Go fix yourself a pina colada while the checks pass and someone reviews your code. 🐸&nbsp;&nbsp;🍍`
+  );
 
-// testx
+// testy
