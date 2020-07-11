8be08fe6930fd6710df13ba246b00c13a1f7f1c1
sidiousvic <victor6rene@gmail.com>
2020-07-11 11:30:55 +0900

    test comment

--- dangerfile.ts
+++ dangerfile.ts
@@ -3,3 +3,5 @@ import { message, danger } from "danger";
 // show modified files in PR
 const modifiedMD = danger.git.modified_files.join("🍍 ");
 message("You have modified these files. \n - " + modifiedMD);
+
+// this is a test.
