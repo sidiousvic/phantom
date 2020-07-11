4fdf87bfbf9545b7cac7a321ecad41c7ed085bfe
sidiousvic <victor6rene@gmail.com>
2020-07-11 12:05:47 +0900

    danger test

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,7 +1,11 @@
 import { markdown, danger } from "danger";
 
 // show modified files in PR
-const modifiedMD = danger.git.modified_files.join("<br>");
-markdown("**You have modified these files.** <br>" + modifiedMD);
+const modifiedMD = danger.git.modified_files
+  .map((fileName) => `🍍 ${fileName}`)
+  .join("<br>");
+markdown(
+  "**<h3>You have been messing with these files.**<h3> <br>" + modifiedMD
+);
 
 // this is y test.
