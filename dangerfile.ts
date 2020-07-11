f152135608dbc04016a8e423598f56373d30f83a
sidiousvic <victor6rene@gmail.com>
2020-07-11 18:52:16 +0900

    danger test

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,25 +1,35 @@
 import { danger, markdown } from "danger";
 
+const userIsAdmin = danger.github.pr.user.login === "sidiousvic";
 const baseBranch = danger.github.pr.base.ref;
-
-// BRANCH PROTECTION
-if (baseBranch === "production") {
-  // PR against production
-  markdown(
-    `<h3>Sick human, you have opened a pull request against <code>${baseBranch}</code>!</h3>Hop outta here before I destroy you with my alien frog lasers! 🐸&nbsp;&nbsp;💥<br></br><!--`
-  );
-} else if (baseBranch === "dev")
-  // PR against dev
-  markdown(
-    `<h3>Gee thanks for your pull request, ribbit!</h3>Go fix yourself a pina colada while the checks pass and someone reviews your code. 🐸&nbsp;&nbsp;🍍<br></br><!--`
-  );
-
-// INCLUDE CHANGELOG WHEN MODIFYING PHANTOM.TS
+const PRAgainstProd = baseBranch === "production";
+const PRAgainstDev = baseBranch === "dev";
 const hasModifiedPhantom = danger.git.modified_files.includes("src/phantom.ts");
 const includesChangelog = danger.git.modified_files.includes("CHANGELOG.md");
 
+// MESSAGES
+
+const YouOpenedAPRAgainstProd =
+  `<h3>Sick human, you have opened a pull request against <code>${baseBranch}</code>!</h3>Hop outta here before I destroy you with my alien frog lasers! 🐸&nbsp;&nbsp;💥<br></br>` +
+  "<!--";
+
+const YouOpenedAPRAgainstDev =
+  `<h3>Gee thanks for your pull request, ribbit!</h3>Go fix yourself a pina colada while the checks pass and someone reviews your code. 🐸&nbsp;&nbsp;🍍<br></br>` +
+  "<!--";
+
+const YouForgotAChangelogFile =
+  `<h3>You forgot CHANGELOG.md!</h3>You've been playing with <code>phantom.ts</code>. Please describe your changes in <code>CHANGELOG.md</code>. 🐸&nbsp;&nbsp;💻<br></br>` +
+  "<!--";
+
+let message = YouOpenedAPRAgainstDev;
+
+// BRANCH PROTECTION
+if (PRAgainstProd && !userIsAdmin) message = YouOpenedAPRAgainstProd;
+else if (PRAgainstDev) message = YouOpenedAPRAgainstDev;
+
+// INCLUDE CHANGELOG WHEN MODIFYING PHANTOM.TS
 if (hasModifiedPhantom && !includesChangelog) {
-  markdown(
-    `<h3>Where's CHANGELOG.md?</h3>You've been playing with <code>phantom.ts</code>, so you got to log your changes in CHANGELOG.md! 🐸&nbsp;&nbsp;⚠️<br></br><!--`
-  );
+  message += YouForgotAChangelogFile;
 }
+
+markdown(message);
