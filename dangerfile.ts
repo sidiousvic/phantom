00735fb3c5b744c7046d70117419108a7339b432
sidiousvic <victor6rene@gmail.com>
2020-07-11 13:02:31 +0900

    testing base branch check

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,11 +1,7 @@
-import { markdown } from "danger";
+import { danger, markdown } from "danger";
 
-// show modified files in PR
-// const modifiedMD = danger.git.modified_files
-//   .map((fileName) => `🍍&nbsp;&nbsp;\`${fileName}\``)
-//   .join("<br>");
+const baseBranch = danger.github.pr.base.ref;
+
 markdown(
-  "<h4>Sick human, you opened a PR against `production`!</h4>Hop outta here before I destroy you with my alien frog lasers! 🐸 💥"
+  `<h3>Sick human, you have opened a PR against <code>${baseBranch}</code>!</h3>Hop outta here before I destroy you with my alien frog lasers! 🐸&nbsp;&nbsp;💥`
 );
-
-// this is x test.
