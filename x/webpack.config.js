2fba859f5f40ff31567267c7253197985c28ae16
sidiousvic <victor6rene@gmail.com>
2020-07-23 08:52:56 +0900

    babel transform ts files

--- x/webpack.config.js
+++ x/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js)$/,
+        test: /\.(js|ts)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
